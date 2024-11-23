bd26f765762932904f489a9d9b41b74da911cbeb
m_skkk <semionykmariasm@gmail.com>
2024-11-23 12:50:18 +0200

    Message from Maria

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    qDebug() << "Hello Sofia";
+
+    //qDebug() << "Hello Sofia";
+    qDebug() << "Message from Maria";
     return a.exec();
 }
